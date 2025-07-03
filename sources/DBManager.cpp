39e45a2d59e069454a3e88027accbf56f619e417
Elouan Geffrelot <elouan.geffrelot@gmail.com>
2025-07-03 16:18:26 +0200

    DBManager class created and db creation and insertion programs refactored.

---
+++ sources/DBManager.cpp
@@ -0,0 +1,33 @@
+#include "DBManager.h"
+
+DBManager::DBManager(const std::string& dbName) {
+    if (sqlite3_open(dbName.c_str(), &this->db)) {
+        std::cerr << "Error opening DB\n";
+        throw std::runtime_error("Database connection failed");
+    }
+}
+
+void DBManager::executeSQL(const char* sql) {
+    char* errMsg = nullptr;
+    if (sqlite3_exec(this->db, sql, nullptr, nullptr, &errMsg) != SQLITE_OK) {
+        std::cerr << "SQL error: " << errMsg << "\n";
+        sqlite3_free(errMsg);
+        throw std::runtime_error("SQL execution failed");
+    }
+}
+
+void DBManager::executeSQL(const std::vector<const char*>& sqlInstructions) {
+    char* errMsg = nullptr;
+    for (const char* sql : sqlInstructions) {
+        if (sqlite3_exec(this->db, sql, nullptr, nullptr, &errMsg) != SQLITE_OK) {
+            std::cerr << "SQL error: " << errMsg << "\n";
+            sqlite3_free(errMsg);
+        }
+    }
+}
+
+DBManager::~DBManager() {
+    if (this->db) {
+        sqlite3_close(this->db);
+    }
+}
