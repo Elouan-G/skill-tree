3b505013ee7529f906d42b1ff702605c875b230b
Elouan Geffrelot <elouan.geffrelot@gmail.com>
2025-07-03 16:32:30 +0200

    Renamed app to generator, more convenient for a first version.

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "1/2 Running app to make tree text."
-./app/skill-tree.out > ./app/tree-text.dot
+echo "1/2 Running generator to make tree text."
+./generator/generator.out > ./generator/tree-text.dot
 echo "2/2 Making image from tree text."
-dot -Tpng ./app/tree-text.dot -o ./app/skill-tree.png
+dot -Tpng ./generator/tree-text.dot -o ./generator/skill-tree.png
 
-open ./app/skill-tree.png &
+open ./generator/skill-tree.png &
 echo "Skill tree visual openned."
